6c43f360a657bbd8e58cedac77a54a380eb051b6
Brendan Weirtz <brendanweirtz@gmail.com>
2022-11-05 19:01:43 -0400

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,11 @@ function App() {
 
   const [color, setColor] = useState("#1fa9f4");
   const [gradient, setGradient] = useState([]);
-  const fontStyles = {
-    Black: {
-        color: 'black'
-    },
-    White: {
-        color: 'white'
-    },
- }
+  
  console.log(typeof color);
   return (
     <div>
+      <div className="left">
       <SketchPicker 
         color={color}
         onChange={(color: any, event: any) => {
@@ -30,32 +24,34 @@ function App() {
           });
         }}
       />
+      </div>
       
       {gradient.map((color, index) => (
       
-      <div key={index} className='color-lables' style={{
-        color: (
-          color[0] == 255 || color[1] == 255 || color[2] == 255) || ( //any color == 255
-          color[0] >190 && color[1] >190 && color[2] >190) || ( //all colors >190
-          color[0] >190 && color[1] >190) || (
-          color[0] >190 && color[2] >190) || (
-          color[1] >190 && color[0] >190) || (
-          color[1] >190 && color[2] >190) || (
-          color[2] >190 && color[0] >190) || (
-          color[2] >190 && color[1] >190)
-          ? 'black' : 'white',
-        backgroundColor: 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')', 
-        padding: "0.5rem",
-        
-      }}>
-        
-        rgb({color[0]}, {color[1]}, {color[2]})
-      </div>
+       <div className="right">
+         <div key={index} className='color-lables' style={{
+            color: ( //change the color of the text based on the background color for readability
+              color[0] == 255 || color[1] == 255 || color[2] == 255) || ( // Any color == 255
+              color[0] > 190 && color[1] > 190 && color[2] > 190) || (       // All colors > 190
+              color[0] > 190 && color[1] > 190) || (                        // red and green > 190
+              color[0] > 190 && color[2] > 190) || (                        // red and blue > 190
+              color[1] > 190 && color[0] > 190) || (                        // green and red > 190 
+              color[1] > 190 && color[2] > 190) || (                        // green and blue > 190
+              color[2] > 190 && color[0] > 190) || (                        // blue and red > 190 
+              color[2] > 190 && color[1] > 190)                             // blue and green > 190
+              ? 'black' : 'white',
+            backgroundColor: 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')', 
+            
+          }}>
+          
+          {/* display color as text in html */}
+          rgb({color[0]}, {color[1]}, {color[2]})
+          <a className="save-color"><div><p>Save color</p></div></a>
+        </div>
+       </div>
       ))}
     </div>
-    
   );
-
 }
 
 export default App;
